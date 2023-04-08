526b69fce136dbc602d074137ada2edd4a4cd4de
Leheza Dmytro <legeza.dmitriy@icloud.com>
2023-04-08 16:31:42 +0300

    delete private information

--- form.js
+++ form.js
@@ -1,89 +1,56 @@
-async function submitForm(event) {
-  event.preventDefault(); // отключаем перезагрузку/перенаправление страницы
-  try {
-    // Формируем запрос
-    console.log(new FormData(event.target));
-    const response = await fetch(event.target.action, {
-      method: "POST",
-      body: new FormData(event.target),
-    });
-    // проверяем, что ответ есть
-    if (!response.ok)
-      throw `Ошибка при обращении к серверу: ${response.status}`;
-    // проверяем, что ответ действительно JSON
-    const contentType = response.headers.get("content-type");
-    if (!contentType || !contentType.includes("application/json")) {
-      throw "Ошибка обработки. Ответ не JSON";
-    }
-    // обрабатываем запрос
-    const json = await response.json();
-    if (json.result === "success") {
-      // в случае успеха
-      alert(json.info);
+// Вибираємо форму та поля форми
+const form = document.querySelector("form");
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const phoneNumber = document.querySelector("#phone-number");
+const email = document.querySelector("#email");
+const businessName = document.querySelector("#business-name");
+const businessAddress1 = document.querySelector("#business-address-1");
+const businessAddress2 = document.querySelector("#business-address-2");
+const city = document.querySelector("#city");
+const stateOrProvince = document.querySelector("#state-or-province");
+const country = document.querySelector("#country");
+const zipOrPostalCode = document.querySelector("#zip-or-postal-code");
+const yearsInBusiness = document.querySelector("#years-in-business");
+const taxId = document.querySelector("#tax-id");
+const website = document.querySelector("#website");
+const otherBrands = document.querySelector("#other-brands");
+const additionalInformation = document.querySelector("#additional-information");
+
+// Додаємо обробник події для надсилання форми на сервер
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Зупиняємо дію за замовчуванням, щоб сторінка не перезавантажувалася
+
+  // Збираємо дані з форми у форматі JSON
+  const formData = JSON.stringify({
+    firstName: firstName.value,
+    lastName: lastName.value,
+    phoneNumber: phoneNumber.value,
+    email: email.value,
+    businessName: businessName.value,
+    businessAddress1: businessAddress1.value,
+    businessAddress2: businessAddress2.value,
+    city: city.value,
+    stateOrProvince: stateOrProvince.value,
+    country: country.value,
+    zipOrPostalCode: zipOrPostalCode.value,
+    yearsInBusiness: yearsInBusiness.value,
+    taxId: taxId.value,
+    website: website.value,
+    otherBrands: otherBrands.value,
+    additionalInformation: additionalInformation.value,
+  });
+
+  // Створюємо AJAX-запит і надсилаємо дані на сервер
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "send.php");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log("Дані успішно надіслані");
     } else {
-      // в случае ошибки
-      console.log(json);
-      throw json.info;
+      console.log("Сталася помилка. Спробуйте ще раз");
     }
-  } catch (error) {
-    // обработка ошибки
-    alert(error);
-  }
-}
-
-// // Вибираємо форму та поля форми
-// const form = document.querySelector("form");
-// const firstName = document.querySelector("#first-name");
-// const lastName = document.querySelector("#last-name");
-// const phoneNumber = document.querySelector("#phone-number");
-// const email = document.querySelector("#email");
-// const businessName = document.querySelector("#business-name");
-// const businessAddress1 = document.querySelector("#business-address-1");
-// const businessAddress2 = document.querySelector("#business-address-2");
-// const city = document.querySelector("#city");
-// const stateOrProvince = document.querySelector("#state-or-province");
-// const country = document.querySelector("#country");
-// const zipOrPostalCode = document.querySelector("#zip-or-postal-code");
-// const yearsInBusiness = document.querySelector("#years-in-business");
-// const taxId = document.querySelector("#tax-id");
-// const website = document.querySelector("#website");
-// const otherBrands = document.querySelector("#other-brands");
-// const additionalInformation = document.querySelector("#additional-information");
-
-// // Додаємо обробник події для надсилання форми на сервер
-// form.addEventListener("submit", function (event) {
-//   event.preventDefault(); // Зупиняємо дію за замовчуванням, щоб сторінка не перезавантажувалася
-
-//   // Збираємо дані з форми у форматі JSON
-//   const formData = JSON.stringify({
-//     firstName: firstName.value,
-//     lastName: lastName.value,
-//     phoneNumber: phoneNumber.value,
-//     email: email.value,
-//     businessName: businessName.value,
-//     businessAddress1: businessAddress1.value,
-//     businessAddress2: businessAddress2.value,
-//     city: city.value,
-//     stateOrProvince: stateOrProvince.value,
-//     country: country.value,
-//     zipOrPostalCode: zipOrPostalCode.value,
-//     yearsInBusiness: yearsInBusiness.value,
-//     taxId: taxId.value,
-//     website: website.value,
-//     otherBrands: otherBrands.value,
-//     additionalInformation: additionalInformation.value,
-//   });
-
-//   // Створюємо AJAX-запит і надсилаємо дані на сервер
-//   const xhr = new XMLHttpRequest();
-//   xhr.open("POST", "send.php");
-//   xhr.setRequestHeader("Content-Type", "application/json");
-//   xhr.onload = function () {
-//     if (xhr.status === 200) {
-//       console.log("Дані успішно надіслані");
-//     } else {
-//       console.log("Сталася помилка. Спробуйте ще раз");
-//     }
-//   };
-//   xhr.send(formData);
-// });
+  };
+  xhr.send(formData);
+});
--- send.php
+++ send.php
@@ -3,22 +3,13 @@
   require 'PHPMailer-master/src/SMTP.php';
   require 'PHPMailer-master/src/Exception.php';
 
-# проверка, что ошибки нет
   if (!error_get_last()) {
-
-    // Переменные, которые отправляет пользователь
-    // $name = $_POST['name'] ;
-    // $email = $_POST['email'];
-    // $text = $_POST['text'];
-    // $file = $_FILES['myfile'];
 
     $name = $_POST["first-name"];
     $last = $_POST["last-name"];
     $phone = $_POST["phone-number"];
     $email = $_POST["email"];
     
-    
-    // Формирование самого письма
     $title = "Заголовок письма";
     $body = "
     <h2>Новий лист</h2>
@@ -28,7 +19,6 @@
     <b>Почта:</b> $email<br><br>
     ";
     
-    // Настройки PHPMailer
     $mail = new PHPMailer\PHPMailer\PHPMailer();
     
     $mail->isSMTP();   
@@ -37,31 +27,22 @@
     //$mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['data']['debug'][] = $str;};
     
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'legezad'; // Логин на почте
-    $mail->Password   = 'gnwaaagidghuaurq'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('legezad@gmail.com', 'Dmitro'); // Адрес самой почты и имя отправителя
+    $mail->Host       = ''; // SMTP сервера вашої пошти
+    $mail->Username   = ''; // Логін на пошті
+    $mail->Password   = ''; // Пароль на пошті
+    $mail->SMTPSecure = '';
+    $mail->Port       = ;
+    $mail->setFrom(''); // Адреса пошти та ім'я відправника
     
-    // Получатель письма
+    // Отримувач листа
     $mail->addAddress('legeza.dmitriy@icloud.com');  
     
-    
-    // Прикрипление файлов к письму
-    // if (!empty($file['name'][0])) {
-    //     for ($i = 0; $i < count($file['tmp_name']); $i++) {
-    //         if ($file['error'][$i] === 0) 
-    //             $mail->addAttachment($file['tmp_name'][$i], $file['name'][$i]);
-    //     }
-    // }
-    // Отправка сообщения
+    // Відправка повідомлення
     $mail->isHTML(true);
     $mail->Subject = $title;
     $mail->Body = $body;    
     
-    // Проверяем отправленность сообщения
+    // Перевіряємо відправленість повідомлення
     if ($mail->send()) {
         $data['result'] = "success";
         $data['info'] = "Сообщение успешно отправлено!";
@@ -77,73 +58,7 @@
     $data['desc'] = error_get_last();
 }
 
-// Отправка результата
+// Овідправка результату
 header('Content-Type: application/json');
 echo json_encode($data);
-  
-  
-  
-  
-  // use PHPMailer\PHPMailer\PHPMailer;
-  // use PHPMailer\PHPMailer\Exception;
-
-  // require "PHPMailer-master/src/PHPMailer.php"
-  // require "PHPMailer-master/src/Exception.php"
-
-  // $mail = new PHPMailer(true);
-  // $mail->CharSet = "UTF-8";
-
-  // $name = $_POST["first-name"];
-  // $last = $_POST["last-name"];
-  // $phone = $_POST["phone-number"];
-  // $email = $_POST["email"];
-
-  // $mail->addAddress("legezad@gmail.com");
-  // $mail->Subject = "Хело еврібаді";
-  // $mail->Body = $name . ' ' . $last . ' ' . $phone . ' ' . $email;
-
-  // $mail->send();
-
-
-
-
-
-
-// require_once('phpmailer/PHPMailerAutoload.php');
-
-// // Отримуємо дані з форми
-// $data = json_decode(file_get_contents('php://input'), true);
-
-// // Відправляємо електронну пошту
-// $mail = new PHPMailer();
-// $mail->CharSet = 'UTF-8';
-// $mail->setFrom('example@example.com', 'My Website');
-// $mail->addAddress('legezad@gmail.com');
-// $mail->isHTML(true);
-// $mail->Subject = 'New Form Submission';
-// $mail->Body = 'First Name: ' . $data['firstName'] . '<br>' .
-//              'Last Name: ' . $data['lastName'] . '<br>' .
-//              'Phone Number: ' . $data['phoneNumber'] . '<br>' .
-//              'Email: ' . $data['email'] . '<br>' .
-//              'Business Name: ' . $data['businessName'] . '<br>' .
-//              'Business Address 1: ' . $data['businessAddress1'] . '<br>' .
-//              'Business Address 2: ' . $data['businessAddress2'] . '<br>' .
-//              'City: ' . $data['city'] . '<br>' .
-//              'State or Province: ' . $data['stateOrProvince'] . '<br>' .
-//              'Country: ' . $data['country'] . '<br>' .
-//              'Zip or Postal Code: ' . $data['zipOrPostalCode'] . '<br>' .
-//              'Years in Business: ' . $data['yearsInBusiness'] . '<br>' .
-//              'Tax ID: ' . $data['taxId'] . '<br>' .
-//              'Website: ' . $data['website'] . '<br>' .
-//              'Other Brands You Sell: ' . $data['otherBrands'] . '<br>' .
-//              'Additional Information: ' . $data['additionalInfo'];
-// if(!$mail->send()) {
-//   http_response_code(500);
-//   echo json_encode(array('message' => 'Помилка при відправці електронної пошти: ' . $mail->ErrorInfo));
-// } else {
-//   http_response_code(200);
-//   echo json_encode(array('message' => 'Дані успішно відправлені!'));
-// }
-
-  
 ?>
